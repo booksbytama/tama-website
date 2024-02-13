8906f65076a7c6569577bc8caa1dbf9899f938d7
kummoona <kummoona@gmail.com>
2024-02-14 00:04:21 +1100

    fix logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,16 @@ const inter = Inter({ subsets: ['latin'] });
 const dm_sans = DM_Sans({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export const metadata: Metadata = {
-  title: 'Tama Portforlio',
+  title: 'Tama Books',
   description: 'Books By Tama',
+  icons: {
+    icon: [
+      {
+        url: '/assets/images/tamalogo.png', // /public path
+        href: '/assets/images/tamalogo.png', // /public path
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
