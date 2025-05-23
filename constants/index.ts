57333e3f0d815e9e609b049d5000ed3f62fcae4d
kummoona <kummoon.ma@gmail.com>
2025-05-23 08:29:25 +0700

    update links to book

--- constants/index.ts
+++ constants/index.ts
@@ -46,6 +46,10 @@ export const allbooks = [
         buylink: 'https://www.amazon.com/dp/B0F3VXRTYK',
       },
       {
+        buyname: 'Buy Ebook (Kindle AU)',
+        buylink: 'https://www.amazon.com.au/dp/B0F3VXRTYK',
+      },
+      {
         buyname: 'Buy Ebook (Google Play)',
         buylink: 'https://play.google.com/store/books/details?id=b3hZEQAAQBAJ',
       },
@@ -53,11 +57,92 @@ export const allbooks = [
         buyname: 'Buy Paperback',
         buylink: 'https://www.amazon.com/dp/1923337033',
       },
+      {
+        buyname: 'Buy Paperback (AU)',
+        buylink: 'https://www.amazon.com.au/dp/1923337033',
+      },
     ],
     sample: 'https://heyzine.com/flip-book/a2ff8d113f.html',
   },
   {
     id: '002',
+    name: 'Starfish Super Squad Book 2 - Coral Reef Heroes',
+    image: 'starfishsupersquadbook2.jpg',
+    slugId: 'starfishsupersquadbook2',
+    shortdescription: 'Starfish Super Squad Book 2 - Coral Reef Heroes',
+    description:
+      'Deep in Coral Cove, two best friends lived under the sea—Shelly, a shy pink starfish, and Spike, a cheerful yellow starfish who loved adventures! One sunny day, they discovered a mysterious treasure map that would change everything. In Book 2, follow Shelly and Spike as they set off on a big adventure and make new friends. But before they can search for treasure, they discover the coral reefs are in trouble!Now, they must work together with their new friends to save the reefs before continuing their quest.',
+
+    longdescription: [
+      'Join Shelly, Spike, and the rest of the Starfish Super Squad in the second book of their 24-book series, as they embark on an exciting mission to protect the coral reefs and uncover even more underwater wonders.',
+    ],
+    ages: '4-8 years',
+    bookType: 'series',
+    seriesname: 'Treasure Map Adventures',
+    serieslink: 'shellyandspike',
+    available: 'yes',
+    buylinks: [
+      {
+        buyname: 'Buy Ebook (Kindler)',
+        buylink: 'https://www.amazon.com/dp/B0D8KX6V12',
+      },
+      {
+        buyname: 'Buy Ebook (Kindler AU)',
+        buylink: 'https://www.amazon.com.au/dp/B0D8KX6V12',
+      },
+      {
+        buyname: 'Buy Paperback',
+        buylink: 'https://www.amazon.com/dp/1923337068',
+      },
+      {
+        buyname: 'Buy Paperback (AU)',
+        buylink: 'https://www.amazon.com.au/dp/1923337068',
+      },
+    ],
+    sample: 'https://heyzine.com/flip-book/6c56778374.html',
+  },
+  {
+    id: '003',
+    shortname: 'Worry Whale',
+    name: 'Worry Whale - A Whimsical Worry Whale: A Tale of Taming Worries',
+    image: 'worrywhale.png',
+    seriesname: '',
+    slugId: 'worrywhale',
+    shortdescription: 'A Whimsical Worry Whale: A Tale of Taming Worries',
+    description:
+      "Dive into a whimsical adventure with a heartwarming children's book that will help your little ones navigate the stormy seas of anxiety. Set sail with Worry Whale, a lovable character who shows children how to transform their worries from heavy anchors into wispy clouds that drift away on the ocean breeze.",
+    longdescription: [
+      'Worry Whale embarks on a journey of self-discovery, learning valuable mindfulness techniques to calm her anxious mind.',
+      'The story celebrates resilience and inner strength, reminding children that even in the face of worries, they have the power to find joy and peace within themselves.',
+    ],
+    ages: '5-8 years',
+    bookType: 'non-series',
+    available: 'yes',
+    sample: 'https://heyzine.com/flip-book/e6f7d6a90e.html',
+    buylinks: [
+      {
+        buyname: 'Buy Ebook (AU Edition)',
+        buylink: 'https://www.amazon.com.au/dp/B0CQVBW877',
+      },
+
+      {
+        buyname: 'Buy Ebook',
+        buylink: 'https://www.amazon.com/dp/B0CQWH9DWC',
+      },
+      {
+        buyname: 'Buy Paperback (AU Edition)',
+        buylink:
+          'https://www.amazon.com.au/Whimsical-Worry-Whale-Taming-Worries/dp/B0CQVQPG4Y',
+      },
+      {
+        buyname: 'Buy Paperback',
+        buylink:
+          'https://www.amazon.com/Whimsical-Worry-Whale-Taming-Worries/dp/B0CQVQPG4Y/',
+      },
+    ],
+  },
+  {
+    id: '004',
     name: 'Marine Life With Billy Book 1',
     image: 'billybook1.jpg',
     seriesname: 'Billy',
@@ -98,7 +183,7 @@ export const allbooks = [
     sample: 'https://heyzine.com/flip-book/32fc0933ac.html',
   },
   {
-    id: '003',
+    id: '005',
     name: 'Buzzy The Bee, A Tale of Triumph on Her First Day at School Book 1',
     image: 'buzzybook1.png',
     seriesname: 'Buzzy',
@@ -127,146 +212,6 @@ export const allbooks = [
           'https://www.amazon.com/Buzzy-Bee-Triumph-First-School/dp/B0D36B6ZF7/',
       },
     ],
-  },
-  {
-    id: '004',
-    shortname: 'Worry Whale',
-    name: 'Worry Whale - A Whimsical Worry Whale: A Tale of Taming Worries',
-    image: 'worrywhale.png',
-    seriesname: '',
-    slugId: 'worrywhale',
-    shortdescription: 'A Whimsical Worry Whale: A Tale of Taming Worries',
-    description:
-      "Dive into a whimsical adventure with a heartwarming children's book that will help your little ones navigate the stormy seas of anxiety. Set sail with Worry Whale, a lovable character who shows children how to transform their worries from heavy anchors into wispy clouds that drift away on the ocean breeze.",
-    longdescription: [
-      'Worry Whale embarks on a journey of self-discovery, learning valuable mindfulness techniques to calm her anxious mind.',
-      'The story celebrates resilience and inner strength, reminding children that even in the face of worries, they have the power to find joy and peace within themselves.',
-    ],
-    ages: '5-8 years',
-    bookType: 'non-series',
-    available: 'yes',
-    sample: 'https://heyzine.com/flip-book/e6f7d6a90e.html',
-    buylinks: [
-      {
-        buyname: 'Buy Ebook (AU Edition)',
-        buylink: 'https://www.amazon.com.au/dp/B0CQVBW877',
-      },
-
-      {
-        buyname: 'Buy Ebook',
-        buylink: 'https://www.amazon.com/dp/B0CQWH9DWC',
-      },
-      {
-        buyname: 'Buy Paperback (AU Edition)',
-        buylink:
-          'https://www.amazon.com.au/Whimsical-Worry-Whale-Taming-Worries/dp/B0CQVQPG4Y',
-      },
-      {
-        buyname: 'Buy Paperback',
-        buylink:
-          'https://www.amazon.com/Whimsical-Worry-Whale-Taming-Worries/dp/B0CQVQPG4Y/',
-      },
-    ],
-  },
-  {
-    id: '005',
-    name: 'Sparky - The Adventures of Sparky the Squirrel: A Tale of Self-Worth',
-    shortname: 'Sparky',
-    image: 'sparky.jpg',
-    seriesname: '',
-    slugId: 'sparky',
-    shortdescription:
-      'The Adventures of Sparky the Squirrel: A Tale of Self-Worth',
-    description:
-      "Meet Sparky, a squirrel who doesn't quite fit the mould. He can't climb the tallest trees or gather the biggest piles of nuts, and that makes him feel small and discouraged. But what if Sparkly's special talents shine differently?",
-    longdescription: [
-      "Join Sparky on a heartwarming adventure as he discovers his hidden gifts! Paint with berries under the sun, tap out tunes on leafy drums, and watch Sparkly's confidence blossom. Along the way, he'll help lost friends, soothe worried hearts with his music, and prove that being different is what makes you amazing!",
-
-      'In The Adventures of Sparky the Squirrel: A Tale of Self-Worth, you will learn to love their unique talents and shine bright! Embark on a heartwarming journey of self-discovery and discover the power of kindness and helping others.',
-      'Enjoy playful rhymes and coluorful illustrations.',
-    ],
-    ages: '4-8 years',
-    bookType: 'non-series',
-    available: 'yes',
-    buylinks: [
-      {
-        buyname: 'Buy Ebook(AU Edition)',
-        buylink: 'https://www.amazon.com.au/dp/B0CQMF94VJ',
-      },
-      {
-        buyname: 'Buy Paperback(AU Edition)',
-        buylink:
-          'https://www.amazon.com.au/Adventures-Sparky-Squirrel-Tale-Self-Worth/dp/B0CQTTBGCL/',
-      },
-      {
-        buyname: 'Buy Ebook',
-        buylink: 'https://www.amazon.com/dp/B0CQNXB8VX',
-      },
-      {
-        buyname: 'Buy Paperback',
-        buylink:
-          'https://www.amazon.com/Adventures-Sparky-Squirrel-Tale-Self-Worth/dp/B0CQTTBGCL/',
-      },
-    ],
-    sample: '',
-  },
-  {
-    id: '006',
-    name: 'Starfish Super Squad Book 2 - Coral Reef Heroes',
-    image: 'starfishsupersquadbook2.jpg',
-    slugId: 'starfishsupersquadbook2',
-    shortdescription: 'Starfish Super Squad Book 2 - Coral Reef Heroes',
-    description:
-      "Dive into an exciting underwater adventure with Shelly and Spike, the best starfish buddies! Armed with a mysterious treasure map, Shelly and Spike embark on a thrilling journey to the Deep Sea. Along the way, they meet a cast of colorful characters, including Billy, the playful fish, and Windy, the gentle giant whale shark.But trouble bubbles up! Prickly sea urchins threaten the vibrant coral reefs. Can Shelly, Spike, and their new friends work together to save the day?With teamwork, bravery, and a dash of humor, they'll discover that even the smallest starfish can make a big difference.",
-
-    longdescription: [
-      'Perfect for young ocean explorers ages 5-8, Starfish Super Squad: Coral Reef Heroes is an exciting read-aloud adventure that includes A captivating underwater adventures, friendship and teamwork, environmental awareness, catchy rhymes, interactive sound words, and beautiful illustrations,',
-    ],
-    ages: '5-8 years',
-    bookType: 'series',
-    seriesname: 'Treasure Map Adventures',
-    serieslink: 'shellyandspike',
-    available: 'yes',
-    buylinks: [
-      {
-        buyname: 'Buy Ebook',
-        buylink: 'https://www.amazon.com/dp/B0D8KX6V12/',
-      },
-      {
-        buyname: 'Buy Paperback',
-        buylink: 'https://www.amazon.com/dp/B0D8P2ZHVQ/',
-      },
-    ],
-    sample: 'https://heyzine.com/flip-book/6c56778374.html',
-  },
-  {
-    id: '007',
-    name: 'Starfish Super Squad Book 3 - The Unhappy Urchin',
-    image: 'starfishsupersquadbook3.jpg',
-    slugId: 'starfishsupersquadbook3',
-    shortdescription: 'Starfish Super Squad Book 3 - The Unhappy Urchi',
-    description:
-      'Join Shelly, the shy pink starfish, and Spike, the adventurous yellow starfish, as they embark on another thrilling underwater quest! After discovering a mysterious treasure map in their previous adventures, our starfish friends are ready for their next big challenge.In this heartwarming tale, Shelly and Spike encounter a grumpy sea urchin named Uma who needs their help. With the aid of their new friend, Billy the blue fish, and the gentle giant whale shark, Windy, our starfish heroes embark on a mission to save the coral reef and make a new friend along the way.',
-
-    longdescription: [
-      'Packed with vibrant illustrations, engaging characters, and a touch of humor, this read-aloud story is perfect for sharing with young children. Explore the wonders of the underwater world, learn about teamwork, and discover the importance of protecting our oceans.',
-    ],
-    ages: '5-8 years',
-    bookType: 'series',
-    seriesname: 'Treasure Map Adventures',
-    serieslink: 'shellyandspike',
-    available: 'yes',
-    buylinks: [
-      {
-        buyname: 'Buy Ebook',
-        buylink: 'https://www.amazon.com/dp/B0D5TNHXXY/',
-      },
-      {
-        buyname: 'Buy Paperback',
-        buylink: 'https://www.amazon.com/dp/B0D9L9KPBP/',
-      },
-    ],
-    sample: 'https://heyzine.com/flip-book/91e1dcc072.html',
   },
 ];
 
