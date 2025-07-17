b2bf3c2b38b5703baa905a505f459dc7188c7f28
kummoona <kummoon.ma@gmail.com>
2025-07-18 09:03:04 +1000

    add loder for gtag

---
+++ pages/_app.js
@@ -0,0 +1,46 @@
+// pages/_app.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as gtag from '../lib/gtag';
+
+const GA_ID = gtag.GA_TRACKING_ID;
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* GA script tag */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
