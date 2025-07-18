322132cbea9ab3d6f7f4500264f93174c6aa785f
kummoona <kummoon.ma@gmail.com>
2025-07-18 10:31:59 +1000

    add google analytics using thirparty lib from nextjs and remove old config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { DM_Sans } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -26,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={dm_sans.className}>{children}</body>
+      <body className={dm_sans.className}>
+        {children} <GoogleAnalytics gaId='G-KX1JZYLVMY' />
+      </body>
     </html>
   );
 }
