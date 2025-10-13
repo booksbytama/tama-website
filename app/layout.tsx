b5cf6fc307559f497fcd98f9145b3b93c5f72457
kummoona <kummoon.ma@gmail.com>
2025-10-13 22:03:33 +1100

    add google analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Fredoka, Quicksand } from 'next/font/google';
 import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -44,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${quicksand.variable} ${fredoka.variable} antialiased`}>
-        {children}
+        {children} <GoogleAnalytics gaId='G-KX1JZYLVMY' />
       </body>
     </html>
   );
